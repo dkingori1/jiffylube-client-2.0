168798bfedca89c2e1a418187af4dbe2a43f0b10
David Kingori <david.kingori@sq1.com>
2016-08-01 15:12:19 -0500

    Add injection task for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,12 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('index', function () {
+  var target = gulp.src('./app/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths: 
+  var sources = gulp.src(['./bower_components/**/*.js', './bower_components/**/*.css'], {read: false});
+ 
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./'));
+});
